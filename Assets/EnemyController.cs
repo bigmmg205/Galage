e4b912b815960e54bddedf6cf76c955439c63f0a
bigmmg205 <146846706+bigmmg205@users.noreply.github.com>
2024-01-11 09:20:01 -0500

    done

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// This class handle Enemy behaviour. It make them walk back & forth as long as they aren't fixed, and then just idle
+/// without being able to interact with the player anymore once fixed.
+/// </summary>
+public class EnemyController : MonoBehaviour
+{
+	// ====== ENEMY MOVEMENT ========
+	public float speed;
+	public float timeToChange;
+	public bool horizontal;
+
+	
+	Rigidbody2D rigidbody2d;
+	float remainingTimeToChange;
+	Vector2 direction = Vector2.right;
+	
+
+	
+	// ================= SOUNDS =======================
+
+	void Start ()
+	{
+		rigidbody2d = GetComponent<Rigidbody2D>();
+		remainingTimeToChange = timeToChange;
+
+
+
+
+	}
+	
+	void Update()
+	{
+		
+		remainingTimeToChange -= Time.deltaTime;
+
+		if (remainingTimeToChange <= 0)
+		{
+			remainingTimeToChange += timeToChange;
+			direction *= -1;
+		}
+
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody2d.MovePosition(rigidbody2d.position + direction * speed * Time.deltaTime);
+	}
+
+
+
+}
